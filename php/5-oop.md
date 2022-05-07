fb0e0891b17a2a4085a5abde9ad16b22e0d26a90
杨晓业 <v_yangxiaoye@tal.com>
2022-05-07 13:28:54 +0800

    面试问题更新

--- php/5-oop.md
+++ php/5-oop.md
@@ -1,4 +1,10 @@
-##1. 写出 php 的 public、protected、private 三种访问控制模式的区别
+##1 写出 php 的 public、protected、private 三种访问控制模式的区别
 public：公有，任何地方都可以访问
 protected：继承，只能在本类或子类中访问，在其它地方不允许访问
 private：私有，只能在本类中访问，在其他地方不允许访问
+
+##2 关于trait
+
+从基类继承的成员会被 trait 插入的成员所覆盖。优先顺序是来自当前类的成员覆盖了 trait 的方法，而 trait 则覆盖了被继承的方法。
+
+
--- test.php
+++ test.php
@@ -14,24 +14,52 @@
 //
 //echo "$a"+"$a"." \n";
 
-$string =
-    "Lorem ipsum dolor sit amet, consectetur
-adipiscing elit. Nunc ut elit id mi ultricies
-adipiscing. Nulla facilisi. Praesent pulvinar,
-sapien vel feugiat vestibulum, nulla dui pretium orci,
-non ultricies elit lacus quis ante. Lorem ipsum dolor
-sit amet, consectetur adipiscing elit. Aliquam
-pretium ullamcorper urna quis iaculis. Etiam ac massa
-sed turpis tempor luctus. Curabitur sed nibh eu elit
-mollis congue. Praesent ipsum diam, consectetur vitae
-ornare a, aliquam a nunc. In id magna pellentesque
-tellus posuere adipiscing. Sed non mi metus, at lacinia
-augue. Sed magna nisi, ornare in mollis in, mollis
-sed nunc. Etiam at justo in leo congue mollis.
-Nullam in neque eget metus hendrerit scelerisque
-eu non enim. Ut malesuada lacus eu nulla bibendum
-id euismod urna sodales. ";
+//$string =
+//    "Lorem ipsum dolor sit amet, consectetur
+//adipiscing elit. Nunc ut elit id mi ultricies
+//adipiscing. Nulla facilisi. Praesent pulvinar,
+//sapien vel feugiat vestibulum, nulla dui pretium orci,
+//non ultricies elit lacus quis ante. Lorem ipsum dolor
+//sit amet, consectetur adipiscing elit. Aliquam
+//pretium ullamcorper urna quis iaculis. Etiam ac massa
+//sed turpis tempor luctus. Curabitur sed nibh eu elit
+//mollis congue. Praesent ipsum diam, consectetur vitae
+//ornare a, aliquam a nunc. In id magna pellentesque
+//tellus posuere adipiscing. Sed non mi metus, at lacinia
+//augue. Sed magna nisi, ornare in mollis in, mollis
+//sed nunc. Etiam at justo in leo congue mollis.
+//Nullam in neque eget metus hendrerit scelerisque
+//eu non enim. Ut malesuada lacus eu nulla bibendum
+//id euismod urna sodales. ";
+//
+//$compressed = gzcompress($string);
+//
+//echo gzuncompress($compressed);
 
-$compressed = gzcompress($string);
+//echo substr("Hello world",-2);
 
-echo gzuncompress($compressed);
+//$str = "Hello world";
+//
+//echo $str[-2];
+
+trait HelloWorld {
+    public function sayHello()
+    {
+        echo 'hello world';
+    }
+}
+
+trait HiMan {
+    public function sayHello()
+    {
+        echo 'hi man';
+    }
+}
+
+class TestSay {
+    use HelloWorld, HiMan;
+}
+
+$tsObj = new TestSay();
+
+$tsObj->sayHello();
